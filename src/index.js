06c348da6fc33bcdb56e88bec1d59796b2f558b5
shreyaansjain <shreyaansjain60@gmail.com>
2021-10-04 13:03:46 +0530

    fix problem

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React, { useRef } from 'react';
+import { render } from 'react-dom';
+import { useReactToPrint } from 'react-to-print';
+import Form from "./components/Form"
+
+const App = () => {
+  const componentRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
+  return (
+    <div>
+      <Form ref={componentRef} />
+      <button onClick={handlePrint}>Save as Pdf</button>
+    </div>
+  );
+};
+render(<App />, document.querySelector('#root'));
